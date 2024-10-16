f82e986801653d120811e80a682f7bb8da39cf32
Md Abdullah <spark8ab@gmail.com>
2024-10-16 19:17:02 +0600

    Calender widget shown

--- lib/easy_event_calendar.dart
+++ lib/easy_event_calendar.dart
@@ -2,5 +2,5 @@
 library easy_event_calender;
 
 /// Exporting the custom calendar controller from the src/controllers directory.
-export 'src/views/easy_event_calendar.dart';
+export 'src/views/easy_event_calendar.dart' show EasyEventCalendar;
 export 'src/data/calendar_event.dart' show CalendarEvent;
